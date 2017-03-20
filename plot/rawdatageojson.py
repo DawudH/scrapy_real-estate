945d9dbcbcc32c06880a1c7655c9201464982091
Dawud Hage <d.d.hage@student.tudelft.nl>
2017-03-20 17:02:31 +0100

    Made the plotting function alot better, see for yourself

---
+++ plot/rawdatageojson.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import math
+import numpy as np
+import geojson
+import re
+
+# From http://wiki.openstreetmap.org/wiki/Mercator#Python
+def toWebMercator(lon, lat):
+
+	r_major=6378137.000
+
+	# Spherical earth
+	y = r_major/2 * math.log((1.0 + math.sin(math.radians(lat))) / (1.0 - math.sin(math.radians(lat))))
+	x = r_major * lon * math.pi/180
+
+
+	return [x, y]
+
+
+# First convert the data to GeoJSON format:
+with open('big_first_run.json', 'r', encoding='utf-8') as scrapy_data_file:
+
+    scrapy_data = pd.read_json(scrapy_data_file)
+    # remove all lists.. this should be done in scrrapy!
+    scrapy_data.loc[:,'BrokerName'] = scrapy_data['BrokerName'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'BuildYear'] = scrapy_data['BuildYear'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'BuildingType'] = scrapy_data['BuildingType'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'City'] = scrapy_data['City'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'LivingArea'] = scrapy_data['LivingArea'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'BrokerName'] = scrapy_data['BrokerName'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'propertyID'] = scrapy_data['propertyID'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'Geolocation'] = scrapy_data['Geolocation'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'Price'] = scrapy_data['Price'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'LotSize'] = scrapy_data['LotSize'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'Street'] = scrapy_data['Street'].apply(lambda x: x[0] if type(x) is list else x)
+    scrapy_data.loc[:,'Zipcode'] = scrapy_data['Zipcode'].apply(lambda x: x[0] if type(x) is list else x)
+
+    # Remove all the duplicates
+    scrapy_data = scrapy_data.drop_duplicates('propertyID')
+
+
+    # GeoJSON format is like:
+
+
+    #    {
+    #       "type": "FeatureCollection",
+    #       "features": [{
+    #           "type": "Feature",
+    #           "geometry": {
+    #               "type": "Point",
+    #               "coordinates": [102.0, 0.5]
+    #           },
+    #           "properties": {
+    #               "prop0": "value0"
+    #           }
+    #       },
+
+    # For more info see: https://tools.ietf.org/html/rfc7946
+
+    geoJSON = '{\n\t "type": "FeatureCollection",\n\t "features": [\n'
+
+    # Loop over the pandas dataframe
+    priceM2 = []
+    for index, row in scrapy_data.iterrows():
+
+        if not isinstance(row['Geolocation'],dict):
+            # There is no geoinfo
+            ## TODO do something smart here
+            continue
+
+		# The coordinates need to be converted to Web Mercator (EPSG:4326 to EPSG:3857)
+        lat = row['Geolocation']['Latitude']
+        lon = row['Geolocation']['Longitude']
+
+		# Sometimes the latitude and longitude are switched around for some reason...
+		# Fortunately we know that they are all in the Netherlands so.. Latitude ~52.3, Longitude ~4.9
+        if lon > lat:
+            # Switch them around!
+            lon = row['Geolocation']['Latitude']
+            lat = row['Geolocation']['Longitude']
+
+		# Do the conversion to UTM-WGS84
+        x, y = toWebMercator(lon,lat)
+
+        if np.isnan(row['LotSize']):
+            row['LotSize'] = row['LivingArea']
+
+
+        # Add a feature (For the coordinates definition see: http://www.macwright.org/2015/03/23/geojson-second-bite.html )
+        geoJSON += '\t\t {\n\t\t\t "type": "Feature", \n\t\t\t "geometry": { \n\t\t\t\t "type": "Point", \n\t\t\t\t "coordinates": [' + str(x) + ', ' + str(y) + '] \n\t\t\t }, \n\t\t\t "properties": { \n\t\t\t\t' + \
+                     '"Price": ' + str(row['Price']) + \
+                     ',\n\t\t\t\t"PPrice": "€{:,}"'.format(row['Price']) + \
+                     ',\n\t\t\t\t"PriceM2": ' + str(int(row['Price']/row['LivingArea'])) + \
+                     ',\n\t\t\t\t"LivingArea": ' + str(row['LivingArea']) + \
+                     ',\n\t\t\t\t"LotSize": ' + str(row['LotSize']) + \
+                     ',\n\t\t\t\t"Street": "' + row['Street'] + '"' +\
+                     ',\n\t\t\t\t"BuildingType": "' + row['BuildingType'] + '"' + \
+                     ',\n\t\t\t\t"BuildYear": ' + str(row['BuildYear']) + \
+                     ',\n\t\t\t\t"City": "' + row['City'].replace('\\u0027','\u0027').title() + '"' + \
+                     ',\n\t\t\t\t"Zipcode": "' + row['Zipcode'] + '"' + \
+                     ' \n\t\t\t } \n\t\t }, \n'
+
+    # Close it
+    # First replace the last ', \n' with ' \n'
+    geoJSON = geoJSON[:-3] + ' \n'
+    geoJSON += '\t]\n}'
+    
+
+    # Validate the file
+    print('Validating generated file..')
+
+    m = geojson.is_valid(geoJSON)
+    print('-- result is it valid? -- ' + m['valid'])
+    print(m['message'])
+
+    #if m['valid'] == 'no':
+    #    raise
+
+    with open('properties.geojson', 'w') as output_file:
+        output_file.write(geoJSON)
--- plot/test_plot.py
+++ plot/test_plot.py
@@ -1,57 +1,47 @@
 from bokeh.io import output_file, show
-from bokeh.models import GeoJSONDataSource
+from bokeh.models import GeoJSONDataSource, HoverTool, LinearColorMapper, WMTSTileSource, LinearColorMapper, ColorBar
 from bokeh.plotting import figure
+from bokeh.palettes import RdYlGn, linear_palette
+from bokeh.tile_providers import STAMEN_TONER, CARTODBPOSITRON
+import matplotlib as mp
+import matplotlib.cm as cm
 
-import json
-import pandas as pd
-
-# First convert the data to GeoJSON format:
-with open('big_first_run.json', 'r', encoding='utf-8') as scrapy_data_file:
-
-    scrapy_data = pd.read_json(scrapy_data_file)
-    # remove all lists.. this should be done in scrrapy!
-    #scrapy_data.loc[:,'BrokerName'] = df['BrokerName'].apply(lambda x: x[0] if type(x) is list else x)
-    #scrapy_data.loc[:,'BuildYear'] = df['BuildYear'].apply(lambda x: x[0] if type(x) is list else x)
-    #scrapy_data.loc[:,'BuildingType'] = df['BuildingType'].apply(lambda x: x[0] if type(x) is list else x)
-    #scrapy_data.loc[:,'City'] = df['City'].apply(lambda x: x[0] if type(x) is list else x)
-    #scrapy_data.loc[:,'LivingArea'] = df['LivingArea'].apply(lambda x: x[0] if type(x) is list else x)
-    #scrapy_data.loc[:,'BrokerName'] = df['BrokerName'].apply(lambda x: x[0] if type(x) is list else x)
-    scrapy_data.loc[:,'propertyID'] = scrapy_data['propertyID'].apply(lambda x: x[0] if type(x) is list else x)
-    scrapy_data.loc[:,'Geolocation'] = scrapy_data['Geolocation'].apply(lambda x: x[0] if type(x) is list else x)
-    scrapy_data.loc[:,'Price'] = scrapy_data['Price'].apply(lambda x: x[0] if type(x) is list else x)
-
-    # Remove all the duplicates
-    scrapy_data = scrapy_data.drop_duplicates('propertyID')
-
-    # GeoJSON format is like:
+cmap = cm.get_cmap('coolwarm')
+palette = [mp.colors.to_hex(cmap(i)) for i in range(256)]
 
 
-    #    {
-    #       "type": "FeatureCollection",
-    #       "features": [{
-    #           "type": "Feature",
-    #           "geometry": {
-    #               "type": "Point",
-    #               "coordinates": [102.0, 0.5]
-    #           },
-    #           "properties": {
-    #               "prop0": "value0"
-    #           }
-    #       },
+tiles = {'OpenMap': WMTSTileSource(url='http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png'),
+         'ESRI': WMTSTileSource(url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{Z}/{Y}/{X}.jpg'),
+         'Wikipedia': WMTSTileSource(url='https://maps.wikimedia.org/osm-intl/{Z}/{X}/{Y}@2x.png'),
+         'Stamen Toner': STAMEN_TONER,
+         'CartoDB Positron': CARTODBPOSITRON}
 
-    # For more info see: https://tools.ietf.org/html/rfc7946
 
-    geoJSON = '{\n\t "type": "FeatureCollection",\n\t "features": [\n'
+with open('properties.geojson','r') as properties_data_file:
+	geo_source = GeoJSONDataSource(geojson=properties_data_file.read())
 
-    # Loop over the pandas dataframe
-    for index, row in scrapy_data.iterrows():
-        
+with open('townships.geojson','r') as townships_data_file:
+	townships_geo_source = GeoJSONDataSource(geojson=townships_data_file.read())
 
-        # Add a feature
-        geoJSON += '\t\t {\n\t\t\t "type": "Feature", \n\t\t\t "geometry": { \n\t\t\t\t "type": "Point", \n\t\t\t\t "coordinates": [' + str(row['Geolocation']['Latitude']) + ', ' + str(row['Geolocation']['Longitude']) + ' \n\t\t\t }, \n\t\t\t "properties": { \n\t\t\t\t "Price": ' + str(row['Price']) + ' \n\t\t\t } \n\t\t }, \n'
+color_mapper = LinearColorMapper(palette=palette, low=1000,high=4000)
 
-    # Close it
-    geoJSON += '\t]\n}'
+hover = HoverTool(tooltips=[('Price','@PPrice'),
+                            ('Address','@Street @Zipcode @City'),
+                            ('Price/m2','€@PriceM2'),
+                            ('BuildingType','@BuildingType')])
 
-    with open('properties.geojson', 'w') as output_file:
-        output_file.write(geoJSON)
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+
+bound = 10000 # meters
+p = figure(title="Real estate in the Netherlands", tools=[TOOLS,hover],x_range=(230000, 1000000), width=1400, height=700, toolbar_location="above")
+p.grid.grid_line_color = None
+p.axis.visible = False
+p.circle(x='x', y='y',alpha=0.9, fill_color={'field': 'PriceM2', 'transform': color_mapper}, source=geo_source)
+color_bar = ColorBar(color_mapper=color_mapper,
+                     label_standoff=12, border_line_color=None, location=(0,0))
+p.add_layout(color_bar, 'right')
+# p.patches('xs', 'ys', fill_alpha=0.1, fill_color='#333333',
+          # line_color='#000000', line_width=0.5, source=townships_geo_source)
+p.add_tile(tiles['CartoDB Positron'])
+output_file("geojson.html")
+show(p)
